8446cb667838825c6f26c4a3929ab3ea5a8fb146
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2018-01-25 11:04:14 +0100

    Add checkstyle

--- src/test/java/com/tactfactory/calculator/AdditionTest.java
+++ src/test/java/com/tactfactory/calculator/AdditionTest.java
@@ -1,12 +1,8 @@
 package com.tactfactory.calculator;
 
-import static org.junit.Assert.*;
-
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class AdditionTest {
